11c38ffdbe6a804b43886d2f20a5f42d7cb18bce
Akinkunmi <akinkunmioye42@gmail.com>
2026-09-08 03:12:10 +0100

    Harden community icon route

--- src/routes/api.community-icons.$id.ts
+++ src/routes/api.community-icons.$id.ts
@@ -2,13 +2,12 @@ import { createFileRoute } from "@tanstack/react-router";
 import { getDb } from "../server/db";
 import { communityIconResponse } from "../server/community-icons";
 import { imageVariant } from "../server/media-images";
-import { endpoint, requireUser } from "../server/http";
+import { endpoint } from "../server/http";
 export const Route = createFileRoute("/api/community-icons/$id")({
   server: {
     handlers: {
       GET: ({ request, params }) =>
         endpoint(async () => {
-          await requireUser(request);
           return communityIconResponse(
             getDb(),
             params.id,
