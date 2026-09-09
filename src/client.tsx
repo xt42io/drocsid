0601b90ec1cb4202bdb2c60e754f506f8925b3e6
Akinkunmi <akinkunmioye42@gmail.com>
2026-09-09 15:03:38 +0100

    Recover from deployment chunk failures

--- src/client.tsx
+++ src/client.tsx
@@ -3,6 +3,11 @@ import "./instrument.client";
 import { StartClient } from "@tanstack/react-start/client";
 import { StrictMode, startTransition } from "react";
 import { hydrateRoot } from "react-dom/client";
+import { reloadAfterChunkFailure } from "./lib/chunk-recovery";
+
+window.addEventListener("vite:preloadError", (event) => {
+  if (reloadAfterChunkFailure()) event.preventDefault();
+});
 
 startTransition(() => {
   hydrateRoot(
